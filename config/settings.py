65d66f2dce982463a8b7129aa28c742d14bd3774
Lucifer <lucifer@localhost.localdomain>
2025-08-06 10:16:49 -0400

    fix: added render domain to allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://ecommerce-api-or-anything.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
