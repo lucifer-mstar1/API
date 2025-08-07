fc1da719951da601c2f2b5b0635cd2bb23174413
Lucifer <lucifer@localhost.localdomain>
2025-08-07 09:51:58 -0400

    Added JWT registration view

--- custom_auth/serializers.py
+++ custom_auth/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
-from .models import CustomUser
+from django.contrib.auth.models import User
 
-class CustomUserSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
-        model = CustomUser
-        fields = ('email', 'password', 'phone_number', 'first_name', 'last_name')
+        model = User
+        fields = ('id', 'username', 'email', 'password')
 
     def create(self, validated_data):
-        return CustomUser.objects.create_user(**validated_data)
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
--- custom_auth/views.py
+++ custom_auth/views.py
@@ -1,28 +1,11 @@
 from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.permissions import AllowAny
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.permissions import AllowAny
-from rest_framework.serializers import ModelSerializer, CharField, ValidationError
+from rest_framework import status
+from .serializers import RegisterSerializer
 
-# Serializer
-class RegisterSerializer(ModelSerializer):
-    password = CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        return user
-
-# View
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny]
@@ -30,15 +13,12 @@ class RegisterView(generics.CreateAPIView):
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-
-        if serializer.is_valid():
-            user = serializer.save()
-
-            refresh = RefreshToken.for_user(user)
-
-            return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-            }, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            "user": serializer.data,
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
